470e251beb2c87e18ce2fd67814a1a53dc8f4cde
Jerimiah Windows <jdgazlay@gmail.com>
2020-06-03 20:21:10 -0500

    add particles for explosion

--- src/scripts/Explosion.gd
+++ src/scripts/Explosion.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+onready var particle : = $Particles2D as Particles2D
 
 func _ready() -> void:
 	set_collision_layer_bit(0, 1)
 	set_collision_mask_bit(1, 1)
+	particle.emitting = true;
 
 
 func _on_AnimationPlayer_animation_finished(_anim_name: String) -> void:
